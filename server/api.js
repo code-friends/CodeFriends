cd3c9824651e93bfbac1b78724478a78535fb56f
Chase Ellsworth <chaseme3@gmail.com>
2014-12-18 15:40:10 -0800

    sets up routers and controllers

--- server/api.js
+++ server/api.js
@@ -2,15 +2,15 @@
 var express = require('express');
 
 //sub-routers
-var authRouter = require('./auth/index.js'); //MAY NEED TO UPDATE BASED ON WHAT PASSPORT ADDS TO AUTH
+// var authRouter = require('./auth/index.js'); //MAY NEED TO UPDATE BASED ON WHAT PASSPORT ADDS TO AUTH
 var projectRouter = require('./project/projectRoutes.js');
 var userRouter = require('./user/userRoutes.js');
 
 //instantiates an instance of the express router
 var apiRouter = express.Router();
 
-apiRouter.use('/auth', authRouter);
-apiRouter.use('/project', projectRouter);
-apiRouter.use('/user', userRouter);
+// apiRouter.use('/auth', authRouter);
+// apiRouter.use('/project', projectRouter);
+// apiRouter.use('/user', userRouter);
 
 module.export = apiRouter;
