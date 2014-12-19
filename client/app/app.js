7251f5930639969d02369bfac0f62d729289ce55
Phung.Doug@gmail.com <phung.doug@gmail.com>
2014-12-18 17:33:36 -0800

    adding codemirror and sharejs

--- client/app/app.js
+++ client/app/app.js
@@ -2,8 +2,15 @@
   console.log('app initializing');
 
   angular.module('code', [
-
+    'ui.router'
     ])
-    .config(function($stateprovider,))
+    .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+      $urlRouterProvider.otherwise('/');
 
+      $stateProvider
+        .state('editor', {
+          url: "/editor", 
+          templateUrl: "app/editor/index.html"
+        })
+    })
 })();
