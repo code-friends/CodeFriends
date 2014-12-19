c113c7aa3f4eaec7a2bd6402e10fa36c4dc9cb4f
Phung.Doug@gmail.com <phung.doug@gmail.com>
2014-12-18 17:33:36 -0800

    adding codemirror and sharejs

--- server/api.js
+++ server/api.js
@@ -6,8 +6,8 @@ module.exports = function router(app){
   app.use('/auth', authRouter);
   app.use('/project', projectRouter);
   app.use('/user', userRouter);
-
   //all other routes go to index.html
+
   app.route('/*')
     .get(function(req, res){
       res.sendfile('client/index.html');
--- server/project/projectRoutes.js
+++ server/project/projectRoutes.js
@@ -3,7 +3,7 @@ var projectController = require('./projectController.js');
 module.exports = function(router){
 
   router.post('/', projectController.PLACEHOLDER );   //NEED TO ADD THE NAME OF THE FUNCTION IN PROJECTCONTROLLER
-  router.get('/', projectController.PLACEHOLDER );    //NEED TO ADD THE NAME OF THE FUNCTION IN PROJECTCONTROLLER
+  router.get('/', app.use() );    //NEED TO ADD THE NAME OF THE FUNCTION IN PROJECTCONTROLLER
   router.post('/', projectController.PLACEHOLDER );   //NEED TO ADD THE NAME OF THE FUNCTION IN PROJECTCONTROLLER
   router.delete('/', projectController.PLACEHOLDER ); //NEED TO ADD THE NAME OF THE FUNCTION IN PROJECTCONTROLLER
 
