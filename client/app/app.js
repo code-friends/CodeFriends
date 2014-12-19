322a76fe41907ffe7ee5ade36daa64215b1d05c4
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-18 16:24:17 -0800

    basic client views for login and home with ui-routing

--- client/app/app.js
+++ client/app/app.js
@@ -2,8 +2,23 @@
   console.log('app initializing');
 
   angular.module('code', [
-
+    'ui.router',
+    'code.login'
     ])
-    .config(function($stateprovider,))
+    .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+        .state('login', {
+          templateUrl: '/app/login/login.html',
+          controller: 'loginController',
+          url: '/login'
+        })
+        .state('home', {
+          templateUrl: '/app/home/home.html',
+          // controller: 'homeController',
+          url: '/home'
+        })
+    });
 
 })();
