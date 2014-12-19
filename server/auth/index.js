c16ec2ba38b40d30bdf5d8f98436beec103e2400
thejsj <jorge.silva@thejsj.com>
2014-12-18 16:04:44 -0800

    Add app routes

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,1 +1,52 @@
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var User = require('../models').User;
+var collections = require('../collections');
 
+passport.serializeUser(function (user, done) {
+  console.log('serializeUser');
+  return done(null, user.get('id'));
+});
+
+passport.deserializeUser(function (id, done) {
+  console.log('deserializeUser');
+  collections.Users
+    .query('where', 'id', '=', id)
+    .fetchOne()
+    .then(function (model) {
+      return done(null, model);
+    });
+});
+
+passport.use(new GitHubStrategy({
+    clientID: '59a42668d2f20b9d11ad',
+    clientSecret: 'c64fb966dd0c8804fd8a8eedbf9638bcad8bbfe2',
+    callbackURL: 'http://localhost:8000'
+  },
+  function (accessToken, refreshToken, profile, done) {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    collections.Users
+      .query('where', 'username', '=', profile.id)
+      .fetchOne()
+      .then(function (user) {
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, null);
+      })
+      .catch(function (err) {
+        return done(null, false);
+      });
+  }
+));
+
+passport.checkIfLoggedIn = function (req, res, next) {
+  if (req.user) {
+    return next();
+  }
+  return res.status(401).send('You\'re not logged in');
+};
+
+module.exports = passport;
--- server/index.js
+++ server/index.js
@@ -11,13 +11,20 @@ var apiRouter = require('./api');
 var db = require('./db');
 
 var port = process.env.PORT || 8000;
+var apiRouter = require('./api');
+var auth = require('./auth');
+//connect to mongoDB
+
+//connect to MySQL
 
 //init app
 var app = express();
 
 //middlewares
 app
-  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
   .use(bodyParser.json())
   .use(morgan('dev'))
   .use(session({
@@ -27,18 +34,10 @@ app
   }));
 
 //set routes
+var port = process.env.PORT || 8000;
 app
   .use(express.static(__dirname + '/../client'))
   .use('/api', apiRouter)
   .listen(port);
 
-console.log('listening on port: ' + port);
-
-//expose app
 module.exports = app;
-
-
-
-
-
-
