2e6e3d923c066f41af6b2697ecba4da5f13ecdb7
Phung.Doug@gmail.com <phung.doug@gmail.com>
2014-12-19 11:55:12 -0800

    server running on 8007

--- client/app/editor/editor.js
+++ client/app/editor/editor.js
@@ -6,13 +6,36 @@ angular.module("code.editor", ['ui.router'])
 
   };
   $scope.runThis = function(){
-  	CodeMirror.fromTextArea( document.getElementById('pad'), {
-  	  value: "function myScript(){return 100;}\n",
-  	  mode:  "javascript", 
-  	  lineNumbers: true
-  	}); 
+  	// CodeMirror.fromTextArea( document.getElementById('pad'), {
+  	//   value: "function myScript(){return 100;}\n",
+  	//   mode:  "javascript", 
+  	//   lineNumbers: true
+  	// }); 
+  	// //cm.setMode('javascript')
+  	// var elem = document.getElementById('pad');
+
+  	var cm = CodeMirror.fromTextArea( document.getElementById('pad'), {
+  		mode: 'javascript', 
+  		value: "alert('hello world')",
+  		lineNumbers: true
+  	} ); 
+
   	//cm.setMode('javascript')
+
   	var elem = document.getElementById('pad');
+  	var ws = new WebSocket('ws://localhost:8007');
+  	// 
+  	var sjs = new window.sharejs.Connection( ws );
+  	// 
+  	var doc = sjs.get( 'users', 'seph' );
+  	// console.log(doc);
+  	doc.subscribe();
+
+  	doc.whenReady(function () {
+  	  if (!doc.type) doc.create('text');
+  	  if (doc.type && doc.type.name === 'text')
+  	  doc.attachCodeMirror(cm);
+  	});
   }
   $scope.runThis(); 
 
