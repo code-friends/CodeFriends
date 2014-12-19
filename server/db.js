157ea38dabe9cf39915cf471d8b1f7203808f100
thejsj <jorge.silva@thejsj.com>
2014-12-18 16:04:44 -0800

    Add app routes

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@
 // });
 
 //mongoDB
-  //insert the straight up mongo
+//insert the straight up mongo
 
 //MySQL
 
@@ -24,36 +24,32 @@ var db = knex({
 });
 
 //users schema
-db.schema.hasTable('users').then(function(exists){
-  if(!exists){
-    db.schema.createTable('users', function(user){
-      user.increments('id').primary();
-      user.string('username', 255);
-      user.timeStamps();
-    })
-    .then(function(table){
-      console.log('created table: users');
-    })
+db.schema.hasTable('users').then(function (exists) {
+  if (!exists) {
+    db.schema.createTable('users', function (user) {
+        user.increments('id').primary();
+        user.string('username', 255);
+        user.timeStamps();
+      })
+      .then(function (table) {
+        console.log('created table: users');
+      });
   }
-})
+});
 
 //projects schema
-db.schema.hasTable('projects').then(function(exists){
-  if(!exists){
-    db.schema.createTable('projects', function(project){
-      project.increments('id').primary();
-      project.string('projectname', 255);
-      project.timeStamps();
-    })
-    .then(function(table){
-      console.log('created table: projects');
-    })
+db.schema.hasTable('projects').then(function (exists) {
+  if (!exists) {
+    db.schema.createTable('projects', function (project) {
+        project.increments('id').primary();
+        project.string('projectname', 255);
+        project.timeStamps();
+      })
+      .then(function (table) {
+        console.log('created table: projects');
+      });
   }
-})
+});
 
 var bookshelf = require('bookshelf')(db);
 module.exports = bookshelf
-
-
-
-
