75a2e7e3b4f0102cf985d86c60ca62d3ca7d8c08
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-19 19:28:45 -0800

    Add mongo to sharejs

--- client/app/projectEditor/editor/editor.js
+++ client/app/projectEditor/editor/editor.js
@@ -1,34 +1,30 @@
+/*global angular:true, CodeMirror:true */
+/*jshint browser:true */
 angular.module('code.editor', ['ui.router'])
-
-.controller('editorController', function ($scope, $state ) {
-  $scope.goToHome = function () {
-    $state.go('home');
-
-  };
-  $scope.runThis = function(){
-
-    var cm = CodeMirror.fromTextArea( document.getElementById('pad'), {
+  .controller('editorController', function ($scope, $state) {
+    $scope.goToHome = function () {
+      $state.go('home');
+    };
+    var cm = CodeMirror.fromTextArea(document.getElementById('pad'), {
       mode: 'javascript',
-      value: "alert('hello world')",
+      value: 'alert(\'hello world\')',
       lineNumbers: true
-    } );
-
+    });
     var elem = document.getElementById('pad');
     var ws = new WebSocket('ws://localhost:8007');
-    var sjs = new window.sharejs.Connection( ws );
-    var doc = sjs.get( 'users', 'seph' );
+    var sjs = new window.sharejs.Connection(ws);
+    var collectionName = 'documents';
+    var documentName = 'doc4';
+    var doc = sjs.get(collectionName, documentName);
     // console.log(doc);
     doc.subscribe();
-
     doc.whenReady(function () {
-      if (!doc.type){
+      console.log(doc);
+      if (!doc.type) {
         doc.create('text');
       }
-      if (doc.type && doc.type.name === 'text'){
+      if (doc.type && doc.type.name === 'text') {
         doc.attachCodeMirror(cm);
       }
     });
-  };
-  $scope.runThis();
-
-});
+  });
