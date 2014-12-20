7040deda4b246a395402e76644f991e8cec28709
Doug Phung <phung.doug@gmail.com>
2014-12-20 15:16:01 -0800

    created projects link to documents

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,12 +1,6 @@
+'use strict';
 /*global angular:true */
 angular.module('code.landing', ['ui.router'])
   .controller('homeController', function ($scope, $state, Auth, $http) {
     Auth.isLoggedIn();
-    $scope.createProject = function () {
-      return $http.post('/api/project', {
-        project_name: $scope.newProjectName
-      }).then(function (res) {
-        return res.data;
-      });
-    };
   });
