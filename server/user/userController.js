af731acb3e946dca8d2d21fb98e506b9346fd97e
Chase Ellsworth <chaseme3@gmail.com>
2014-12-19 20:28:48 -0800

    adds server logic 4

--- server/user/userController.js
+++ server/user/userController.js
@@ -1,5 +1,5 @@
-var userModel = require('./userModel.js');
 var express = require('express');
+var userModel = require('./userModel.js');
 var models = require('../models.js').models;
 var collections = require('../models.js').collections;
 
@@ -9,8 +9,15 @@ userController.post = function (req, res) {
 	res.status(200).end();
 };
 
-userController.get = function (req, res) {
-	res.status(200).end();
+userController.getAllUsers = function (req, res) {
+	console.log('got into getAllUsers');
+	models.User
+		.fetchAll({
+			withRelated: ['project']
+		})
+		.then(function (coll) {
+			res.json(coll.toJSON()).end();
+		})
 };
 
 userController.put = function (req, res) {
