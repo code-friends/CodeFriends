90357ddcb37caa5535c8ef19b9f402ac73a51391
thejsj <jorge.silva@thejsj.com>
2014-12-19 17:13:20 -0800

    Add GitHub Login

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,45 +1,57 @@
 var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
-// var User = require('../user/userModel').User;
-// var collections = require('../collections');
+var UserCollection = require('../models').collections.UserCollection;
 
 passport.serializeUser(function (user, done) {
-  console.log('serializeUser');
   return done(null, user.get('id'));
 });
 
 passport.deserializeUser(function (id, done) {
-  console.log('deserializeUser');
-  // collections.Users
-  //   .query('where', 'id', '=', id)
-  //   .fetchOne()
-  //   .then(function (model) {
-  //     return done(null, model);
-  //   });
+  new UserCollection()
+    .query('where', 'id', '=', id)
+    .fetchOne()
+    .then(function (model) {
+      return done(null, model);
+    });
 });
 
 passport.use(new GitHubStrategy({
-    clientID: '59a42668d2f20b9d11ad',
-    clientSecret: 'c64fb966dd0c8804fd8a8eedbf9638bcad8bbfe2',
-    callbackURL: 'http://127.0.0.1:8000'
+    clientID: '364ea3bc2b086177fd27',
+    clientSecret: '2dce4e81ad618474f5c822b4567200b941a6c1b1',
+    callbackURL: 'http://127.0.0.1:8000/auth/login/callback'
   },
   function (accessToken, refreshToken, profile, done) {
-    console.log('passport GitHubStrategy');
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
-    // collections.Users
-    //   .query('where', 'username', '=', profile.id)
-    //   .fetchOne()
-    //   .then(function (user) {
-    //     if (user) {
-    //       return done(null, user);
-    //     }
-    //     return done(null, null);
-    //   })
-    //   .catch(function (err) {
-    //     return done(null, false);
-    //   });
+    // I'm not exactly sure when we use an accessToken and a refreshToken
+    if (accessToken !== null) {
+      new UserCollection()
+        .query('where', 'githubAccessToken', '=', accessToken)
+        .fetchOne()
+        .then(function (user) {
+          if (user) {
+            done(null, user);
+            return user;
+          }
+          return new UserCollection()
+            .create({
+              username: profile._json.login,
+              githubId: profile._json.id,
+              githubName: profile._json.name,
+              githubEmail: profile._json.email,
+              githubLocation: profile._json.location,
+              githubAccessToken: accessToken,
+              githubAvatarUrl: profile._json.avatar_url
+            })
+            .then(function (user) {
+              if (user) {
+                return done(null, user);
+              }
+              return done(null, null);
+            });
+        })
+        .catch(function (err) {
+          return done(null, false);
+        });
+    }
   }
 ));
 
