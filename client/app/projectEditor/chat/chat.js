4125e8504f6c25a99ed3549bca6e2f4b3c3fa1ed
Doug Phung <phung.doug@gmail.com>
2014-12-27 16:55:22 -0800

    removed a lot of console logs from chats

--- client/app/projectEditor/chat/chat.js
+++ client/app/projectEditor/chat/chat.js
@@ -1,14 +1,11 @@
 angular.module('code.chat', ['ui.router'])
   .controller('chatController', function ($scope, $state, ngSocket, $stateParams) {
-    console.log($stateParams.docID);
     var roomID = $stateParams.docID;
     $scope.roomID = roomID;
-    console.log("this is the room ID", roomID);
     var ws = ngSocket('ws://localhost:8001');
     $scope.messages = [];
     ws.onMessage(function (msg) {
       msg = JSON.parse(msg.data);
-      console.log(msg.message);
       if (msg.message.hasOwnProperty(roomID)) {
         $scope.messages.push(msg);
       }
