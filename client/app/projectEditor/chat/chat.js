2835a6948c22a78c413495dddc221237523ef11f
Doug Phung <phung.doug@gmail.com>
2014-12-27 16:53:40 -0800

    added chat. chat only pushes to other like rooms

--- client/app/projectEditor/chat/chat.js
+++ client/app/projectEditor/chat/chat.js
@@ -1,21 +1,22 @@
 angular.module('code.chat', ['ui.router'])
-  .controller('chatController', function ($scope, $state, ngSocket) {
+  .controller('chatController', function ($scope, $state, ngSocket, $stateParams) {
+    console.log($stateParams.docID);
+    var roomID = $stateParams.docID;
+    $scope.roomID = roomID;
+    console.log("this is the room ID", roomID);
     var ws = ngSocket('ws://localhost:8001');
     $scope.messages = [];
     ws.onMessage(function (msg) {
       msg = JSON.parse(msg.data);
-      console.log('message received', msg);
-      $scope.messages.push(msg);
+      console.log(msg.message);
+      if (msg.message.hasOwnProperty(roomID)) {
+        $scope.messages.push(msg);
+      }
     });
-    ws.send({
-      foo: 'bar'
-    });
-
     $scope.doSomething = function () {
-      console.log($scope.chatMessage);
-      ws.send({
-        foo: $scope.chatMessage
-      });
+      var params = {};
+      params[roomID] = $scope.chatMessage;
+      ws.send(params);
       $scope.chatMessage = '';
     };
 
