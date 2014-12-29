df534d8f02851498fa62901b4ff4858397f0cfa7
Chase Ellsworth <chaseme3@gmail.com>
2014-12-29 11:06:28 -0800

    creates put request 1

--- server/project/projectController.js
+++ server/project/projectController.js
@@ -3,12 +3,12 @@ var models = require('../models.js').models;
 var collections = require('../models.js').collections;
 var Promise = require('bluebird');
 
-
 var projectController = {};
 
+//ADDS A NEW PROJECT AND ADDS THE CREATOR TO THE 'USER' PROPERTY
 projectController.post = function (req, res) {
-  //// THIS IS THE RIGHT ONE ONCE AUTH IS ATTACHED. DUMMY DATA BELOW ////////////////
   // var userId = req.user.get('id');
+  // ABOVE IS THE RIGHT ONE ONCE AUTH IS ATTACHED. DUMMY DATA BELOW
   var userId = {
     id: 2
   };
@@ -24,16 +24,18 @@ projectController.post = function (req, res) {
     })
     .save()
     .then(function (model) {
-      console.log('MODEL1 !!!!!!!!!!!!!!!!', model);
       return model.related('user').create(userId).yield(model);
     })
     .then(function (model) {
-      console.log('model2 !!!!!!!!!!!!!!!!', model);
       res.json(model.toJSON());
     })
 };
 
 projectController.getAllProjects = function (req, res) {
+  // var userId = req.user.get('id');
+
+  //only allow access to the file for projects associated with this current user (they only have permission for those)
+
   models.Project
     .fetchAll({
       withRelated: ['user']
@@ -43,10 +45,38 @@ projectController.getAllProjects = function (req, res) {
     });
 };
 
-//MAKE SEARCHABLE BY EITHER ID OR NAME
-projectController.getSpecificProject = function (req, res) {
+projectController.getSpecificProjectByName = function (req, res) {
+  // var userId = req.user.get('id');
+  //if user is one of the users in if the project
+  //////then execute the code below
+
   models.Project
-    .query('where', 'project_name', '=', req.params.project_name)
+    .query({
+      where: {
+        project_name: req.params.project_name
+      }
+    })
+    .fetch({
+      withRelated: ['user']
+    })
+    .then(function (coll) {
+      res.send(coll);
+    });
+};
+// .query('where', 'project_name', '=', req.params.project_name)
+
+projectController.getSpecificProjectById = function (req, res) {
+  // var userId = req.user.get('id');
+
+  //if user is one of the users in if the project
+  //////then execute the code below
+
+  models.Project
+    .query({
+      where: {
+        id: req.params.id
+      }
+    })
     .fetch({
       withRelated: ['user']
     })
@@ -55,28 +85,50 @@ projectController.getSpecificProject = function (req, res) {
     });
 };
 
-
-//HOW WILL WE FIND OUT find out if they have permission?
-//ALWAYS USER THE PROJECT / USER IDs?
+//HOW WILL WE FIND OUT find out if they have permission?, //ALWAYS USER THE PROJECT / USER IDs?
 //GET
 ////get specific users or projects by the name OR the id
 ////req.user is an express way of saying who did the request. so the userid of the person to add upon creation is
-////
-////
-//PUT
-////add user to a project
 ////add project to a user
-////remove users from a project
 ////remove project from a user
 //DELETE
 //delete a user
-//delete a project
 
+
+////ADD USER TO A PROJECT
+projectController.addUser = function (req, res) {
+  // var userId = req.user.get('id');
+  var project = req.body.project_name;
+  var newUser = req.params.newUser;
+  console.log('NEW USERS !!!!!!!!!!!', newUser);
+  console.log('REQ IN THE ADDUSER!!!!!!!!!!!!!!!!!!', req)
+
+  models.Project
+    .query({
+      where: {
+        project_name: req.body.project_name
+      }
+    })
+    .fetch({
+      withRelated: ['user']
+    })
+    .then(function (coll) {
+      res.send(coll);
+    });
+};
+
+////REMOVE USERS FROM A PROJECT
 projectController.put = function (req, res) {
+  // var userId = req.user.get('id');
   res.status(200).end();
 };
 
+//DELETE A PROJECT
 projectController.delete = function (req, res) {
+  // var userId = req.user.get('id');
+
+  //if user is one of the users in if the project
+  //////then execute the code below
   res.status(200).end();
 };
 
