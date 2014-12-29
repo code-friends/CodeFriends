9fa023c659ea7e3f0cf25696a617e31e068f235e
thejsj <jorge.silva@thejsj.com>
2014-12-29 15:50:11 -0800

    Add test to fileController

--- server/file/fileController.js
+++ server/file/fileController.js
@@ -1,47 +1,64 @@
+'use strict';
 var Promise = require('bluebird');
 var mongoClient =  Promise.promisifyAll(require('mongodb').MongoClient);
 var Q = require('q');
+var moment = require('moment');
 
 var ProjectCollection = require('../models').collections.ProjectCollection;
-var Project = require('../models').models.Project;
+// var Project = require('../models').models.Project;
 
 var fileController = {
   createNewFile: function (req, res) {
     console.log('CREATE NEW FILE');
     var userId = req.user.get('id');
-    var projectName = req.body.project_name || req.param('project_name')
+    var projectName = req.body.project_name || req.param('project_name');
     var fileName = req.body.file_name || req.param('file_name');
     var projectId = req.body.project_id || req.param('project_id') || null;
     // path: req.body.path || req.param('path') || ''
-    console.log(userId, projectName, projectId, fileName);
+    console.log('UserId:', userId, 'ProjectName:', projectName, 'ProjectId:', projectId, 'FileName:', fileName);
 
-    // Check if name is valid
-      // Only A-z0-9_- (no spaces, no whitespace)
+    // Check if name is valid (no white space)
+    if (/\s/g.test(fileName)) {
+      return res.status(400).send('Invalid File Name').end();
+    }
 
     fileController.getFileStructure(projectId, projectName)
       .then(function (fileStructure) {
         console.log('File Structure');
         console.log(fileStructure);
-
-        // If path is not empty
+        // TODO: If path is not empty
           // Check if path exists
-        if (fileStructure === null) {
-          return mongoClient.connectAsync('mongodb://localhost:27017/codeFriends?auto_reconnect')
-            .then(function (db) {
-              var projectCollection = Promise.promisifyAll(db.collection('project_file_structre'));
-              projectCollection.insertAsync({project_id: project.get('id'), files: [fileName]});
-            });
-        }
-        // Create Object with author, timeCreated
-        // Update file structure for whole project in mongo
+
+        return mongoClient.connectAsync('mongodb://localhost:27017/codeFriends?auto_reconnect')
+          .then(function (db) {
+            var projectCollection = Promise.promisifyAll(db.collection('project_file_structre'));
+            // Create Object with author, timeCreated
+            var files = fileStructure.files;
+            if (fileStructure.files.indexOf(fileName) === -1) {
+              var mysqlFormat = 'YYYY-MM-DD HH:MM:SS';
+              files.push({
+                name: fileName,
+                created: moment().format(mysqlFormat),
+                author: req.user.get('id')
+              });
+              // Update file structure for whole project in mongo
+              return projectCollection.updateAsync({_id: fileStructure._id }, {$set: {files: files}}, {w: 1})
+                .then(function () {
+                  return projectCollection.findOneAsync({_id: fileStructure._id});
+                });
+            }
+          })
+          .then(function (fileStructure) {
+            return res.json(fileStructure).end();
+          });
       })
       .catch(function (error) {
-        console.log('Error:', error)
+        console.log('Error:', error);
         res.status(400).end();
       });
   },
   getFileStructure: function (projectId, projectName) {
-    return Q().then(function () {
+    return new Q().then(function () {
       if (projectId !== null && projectId !== undefined) { // If project ID
         // Check if project ID exists
         return ProjectCollection
@@ -55,7 +72,7 @@ var fileController = {
           .query('where', 'project_name', '=', projectName)
           .fetchOne();
       }
-      throw new Error('No Project ID or name specified')
+      throw new Error('No Project ID or name specified');
     })
     .then(function (project) {
       // Get project structure form mongo
@@ -63,6 +80,13 @@ var fileController = {
         .then(function (db) {
           var projectCollection = Promise.promisifyAll(db.collection('project_file_structre'));
           return projectCollection.findOneAsync({project_id: project.get('id')})
+            .then(function (projectFileStructure) {
+              // Create empty project if nothing is found
+              if (projectFileStructure === null) {
+                return projectCollection.insertAsync({project_id: project.get('id'), files: []});
+              }
+              return projectFileStructure;
+            })
             .then(function (projectFileStructure) {
               console.log('projectFileStructure:', projectFileStructure);
               db.close();
--- server/tests/index.js
+++ server/tests/index.js
@@ -1,2 +1,3 @@
+'use strict';
 require('./db.tests.js');
 require('./integration');
