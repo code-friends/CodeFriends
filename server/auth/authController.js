0400d1d5b069277671baff5fdc5af09a06999543
thejsj <jorge.silva@thejsj.com>
2014-12-29 10:26:23 -0800

    Adding passport-local auth

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -1,4 +1,6 @@
 var authController = {};
+var UserCollection = require('../models').collections.UserCollection;
+var User = require('../models').models.User;
 
 authController.getUser = function (req, res) {
   var userId = null;
@@ -13,6 +15,36 @@ authController.getUser = function (req, res) {
 authController.logout = function (req, res) {
   res.logout();
   res.status(200).end();
+};
+
+authController.login = function (req, res) {
+  res.redirect('/#/home');
+};
+
+authController.signup = function (req, res) {
+  var email = req.body.email || req.param('email');
+  var password = req.body.password || req.param('password');
+  if (!email || !password) {
+    res.status(400).end(); // Client Error
+    return;
+  }
+
+  new UserCollection()
+    .query('where', 'email', '=', email)
+    .fetchOne()
+    .then(function (user) {
+      if (user !== null) {
+        res.redirect('/#/home');
+        return;
+      }
+      var user = new User({
+          email: email,
+          password: password
+        }).save()
+        .then(function (userModel) {
+          res.redirect('/#/home');
+        });
+    });
 };
 
 module.exports = authController;
