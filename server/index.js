770b66851b6cfc30d8157f3728c2a253d0724c91
Doug Phung <phung.doug@gmail.com>
2014-12-29 17:23:14 -0800

    refactored server/index.js with jorge

--- server/index.js
+++ server/index.js
@@ -4,95 +4,9 @@ var bodyParser = require('body-parser'),
   morgan = require('morgan'),
   marked = require('marked'),
   session = require('express-session'),
-  express = require('express');
-
-var liveDB = require('livedb-mongo');
-
-// shareJS and codemirror dependencies
-var connect = require('connect'),
-  http = require('http'),
-  argv = require('optimist').argv,
-  // serveStatic = require( 'serve-static' ), not sure if we need this.
-  shareCodeMirror = require('share-codemirror'),
-  Duplex = require('stream').Duplex,
-  livedb = require('livedb'),
-  sharejs = require('share'),
-  shareJSPort = argv.p || 8007,
-  shareJSapp = connect(),
-  server = http.createServer(shareJSapp),
-  // No credentials passed on to MongoDB (might need to change)
-  db = liveDB('mongodb://localhost:27017/codeFriends?auto_reconnect', {
-    safe: true
-  }),
-  // For memory-stored livedb, use livedb.memory()
-  backend = livedb.client(db),
-  share = sharejs.server.createClient({
-    backend: backend
-  }),
-  WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({
-    server: server
-  });
-
-var chatPort = 8001,
-  chatApp = connect(),
-  chatServer = http.createServer(chatApp),
-  chatWS = new WebSocketServer({
-    server: chatServer
-  })
-
-chatWS.on('connection', function (ws) {
-  ws.on('message', function (msg) {
-    console.log(msg);
-    chatWS.broadcast(msg);
-  });
-  ws.send('something from the server');
-});
-
-chatWS.broadcast = function broadcast(data) {
-  for (var i in this.clients) {
-    this.clients[i].send(data);
-  }
-};
-
-chatServer.listen(chatPort);
-
-
-
-server.listen(shareJSPort);
-console.log('editor listening on http://localhost:' + shareJSPort + '/');
-
-// Create editor ws connection
-wss.on('connection', function (client) {
-  var stream = new Duplex({
-    objectMode: true
-  });
-  stream._write = function (chunk, encoding, callback) {
-    console.log('s->c ', chunk);
-    client.send(JSON.stringify(chunk));
-    return callback();
-  };
-  stream._read = function () {};
-  stream.headers = client.upgradeReq.headers;
-  stream.remoteAddress = client.upgradeReq.connection.remoteAddress;
-  client.on('message', function (data) {
-    console.log('c->s ', data);
-    return stream.push(JSON.parse(data));
-  });
-  stream.on('error', function (msg) {
-    return client.close(msg);
-  });
-  client.on('close', function (reason) {
-    stream.push(null);
-    stream.emit('close');
-    console.log('client went away');
-    return client.close(reason);
-  });
-  stream.on('end', function () {
-    return client.close();
-  });
-  return share.listen(stream);
-});
+  express = require('express'),
+  chatServer = require('./chatServer.js'),
+  shareJSServer = require('./sharejs/shareJSServer');
 
 // Set routes
 var auth = require('./auth');
@@ -123,6 +37,9 @@ app
 
 //set routes
 var port = process.env.PORT || 8000;
+var shareJSPort = 8007;
+var chatPort = 8001;
+
 app
   .use(express.static(__dirname + '/../client'))
   .use('/auth', authRouter)
@@ -130,5 +47,11 @@ app
   .listen(port, function () {
     console.log('Server listening on port: ', port);
   });
+
+chatServer.listen(chatPort);
+shareJSServer.listen(shareJSPort);
+
+console.log('Chat listening on port:', chatPort);
+console.log('Editor listening on port:', shareJSPort);
 
 module.exports = app;
