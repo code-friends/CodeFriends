873a17a30fadea40ba840e0b97fc374581f8a138
Doug Phung <phung.doug@gmail.com>
2014-12-29 17:23:14 -0800

    refactored server/index.js with jorge

---
+++ server/sharejs/socketHandler.js
@@ -0,0 +1,47 @@
+'use strict';
+var sharejs = require('share');
+var Duplex = require('stream').Duplex;
+var liveDBMongoClient = require('livedb-mongo');
+var livedb = require('livedb');
+var db = liveDBMongoClient('mongodb://localhost:27017/codeFriends?auto_reconnect', {
+	safe: true
+});
+var backend = livedb.client(db);
+var share = sharejs.server.createClient({
+	backend: backend
+});
+
+
+var socketConnectionHandler = function (client) {
+	var stream = new Duplex({
+		objectMode: true
+	});
+	stream._write = function (chunk, encoding, callback) {
+		console.log('s->c ', chunk);
+		client.send(JSON.stringify(chunk));
+		return callback();
+	};
+	stream._read = function () {};
+	stream.headers = client.upgradeReq.headers;
+	stream.remoteAddress = client.upgradeReq.connection.remoteAddress;
+
+	client.on('message', function (data) {
+		console.log('c->s ', data);
+		return stream.push(JSON.parse(data));
+	});
+	stream.on('error', function (msg) {
+		return client.close(msg);
+	});
+	client.on('close', function (reason) {
+		stream.push(null);
+		stream.emit('close');
+		console.log('client went away');
+		return client.close(reason);
+	});
+	stream.on('end', function () {
+		return client.close();
+	});
+	return share.listen(stream);
+};
+
+module.exports = socketConnectionHandler;
