8073868f3d18626332e3a76ed75441e11ebeb71d
Chase Ellsworth <chaseme3@gmail.com>
2014-12-29 20:50:59 -0800

    connects server to client 1

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -1,7 +1,7 @@
 /*global angular:true */
 angular.module('code.login', ['ui.router'])
-  .controller('loginController', function ($scope, $state, Auth) {
-    console.log('Showing Login.js');
-    Auth.isLoggedIn();
-    // Silence is Beautiful
-  });
+	.controller('loginController', function ($scope, $state, AuthFactory) {
+		console.log('Showing Login.js');
+		AuthFactory.isLoggedIn();
+		// Silence is Beautiful
+	});
