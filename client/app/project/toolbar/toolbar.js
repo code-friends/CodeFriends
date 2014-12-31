6b70a4313ca5deec0a6a95fb6f18d13c38db74bb
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-30 19:56:04 -0800

    Add toolbar html and controllerjs

--- client/app/project/toolbar/toolbar.js
+++ client/app/project/toolbar/toolbar.js
@@ -1,19 +1,18 @@
 /*global angular:true */
 angular.module('code.toolbar', [])
   .controller('toolbarController', function ($scope, $state, $stateParams, $http) {
-    console.log('toolbar controller initialized!');
+      console.log('toolbar controller initialized!');
 
-    $scope.addUser = function () {
-      console.log('called addUser()');
-      console.log('$scope.newUser !!!!!!!!!', $scope.newUser);
-      console.log('$stateParams.projectName !!!!!!!!!!', $stateParams.projectName);
-      return $http.put('api/project/addUser', {
-          newUserName: $scope.newUser,
-          project_name: $stateParams.projectName
-        })
-        .catch(function (error) {
-          console.log('error!!!!', error);
-          // console.log('Added User');
-        });
-    };
-  });
+      $scope.addUser = function () {
+        console.log('called addUser()');
+        console.log('$scope.newUser !!!!!!!!!', $scope.newUser);
+        console.log('$stateParams.projectName !!!!!!!!!!', $stateParams.projectName);
+        return $http.put('api/project/addUser', {
+            newUserName: $scope.newUser,
+            project_name: $stateParams.projectName
+          })
+          .catch(function (error) {
+            console.log('error!!!!', error);
+            // console.log('Added User');
+          });
+      };
