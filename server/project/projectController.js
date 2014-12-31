32cb822acee24694be47b9dd1ab3601b0146ef3c
Chase Ellsworth <chaseme3@gmail.com>
2014-12-30 19:19:04 -0800

    integrates server and client 1

--- server/project/projectController.js
+++ server/project/projectController.js
@@ -15,10 +15,9 @@ projectController.post = function (req, res) {
   console.log('THIS IS REQ IN PROJECT.CONTROLLER.POST !!!!', req);
 
   var userId = req.user.get('id');
-
   var project_name = req.body.project_name;
 
-  if (!project_name) {
+  if (!project_name || !userId) {
     res.status(400).end();
   }
   new models.Project({
@@ -42,7 +41,12 @@ projectController.post = function (req, res) {
 /////////////////////////////////////////    GET    /////////////////////////////////////////
 ///   var userId = req.user.get('id');   ///   only allow access to the file for projects associated with this current user (they only have permission for those)
 projectController.getAllProjects = function (req, res) {
-  models.Project
+  var userId = req.user.get('id');
+  models.Project.query({
+      where: {
+        id: req.user.id
+      }
+    })
     .fetchAll({
       withRelated: ['user']
     })
@@ -78,86 +82,91 @@ projectController.getSpecificProject = function (req, res) {
 /////////////////////////////////////////    PUT    /////////////////////////////////////////
 //ADD USER TO A PROJECT   ///   var userId = req.user.get('id');   ///   BELOW IS HARD CODED. NEED TO CHANGE
 projectController.addUser = function (req, res) {
-  console.log('REQ !!!!!!!!!', req.body);
-  var project_name = req.body.project_name;
-  var newUserName = req.body.newUserName;
-  var newUser = models.User
-    .query({
-      where: {
-        username: newUserName
-      }
-    })
-    .fetch({
-      withRelated: ['project']
-    })
-    .then(function (model) {
-      if (!model) {
-        console.log('DUDE, THERE IS NO MODEL WITH THAT NAME !!!!!!!!');
-      }
-      console.log('ID !!!!', model.get('id'));
-    })
-    .catch(function (err) {
-      console.log('Error adding user', err);
-    });
+    console.log('REQ !!!!!!!!!', req.body);
+    var project_name = req.body.project_name;
+    var newUserName = req.body.newUserName;
+    var newUser = models.User
+      .query({
+        where: {
+          username: newUserName
+        }
+      })
+      .fetch({
+        withRelated: ['project']
+      })
+      .then(function (model) {
+        if (!model) {
+          console.log('DUDE, THERE IS NO MODEL WITH THAT NAME !!!!!!!!');
+        }
+        console.log('ID !!!!', model.get('id'));
+      })
+      .catch(function (err) {
+        console.log('Error adding user', err);
+      });
 
 
-  var user = {
-    id: newUser
-  };
+    /////////////////////////////////////////    PUT    /////////////////////////////////////////
+    //ADD USER TO A PROJECT   ///   var userId = req.user.get('id');  
+    projectController.addUser = function (req, res) {
+      var project_name = req.body.project_name;
+      var userId = req.user.get('id');
+      // var userId = {
+      //   id: req.body.userId
+      // };
 
-  models.Project
-    .query({
-      where: {
-        project_name: project_name
-      }
-    })
-    .fetch({
-      withRelated: ['user']
-    })
-    .then(function (model) {
-      return model
-        .related('user')
-        .create(user)
-        .yield(model)
-        .catch(function (err) {
-          console.log('Error adding user', err);
+      models.Project
+        .query({
+          where: {
+            project_name: project_name
+          }
+        })
+        .fetch({
+          withRelated: ['user']
+        })
+        .then(function (model) {
+          return model
+            .related('user')
+            .create(userId)
+            .yield(model)
+            .catch(function (err) {
+              console.log('Error adding user', err);
+            });
+        })
+        .then(function (model) {
+          res.json(model.toJSON());
         });
-    })
-    .then(function (model) {
-      res.json(model.toJSON());
-    });
-};
+    };
 
-//REMOVE USERS FROM A PROJECT   ///   if user is one of the users in if the project   ///   then execute the code below
-projectController.put = function (req, res) {
-  // var userId = req.user.get('id');
-  res.status(200).end();
-};
-
-/////////////////////////////////////////    DELETE    /////////////////////////////////////////
-//DELETE A PROJECT
-projectController.delete = function (req, res) {
-  models.Project
-    .query({
-      where: {
-        id: req.body.id
-      }
-    })
-    .fetch()
-    .then(function (model) {
-      return db('projects_users')
-        .where('project_id', '=', model.get('id'))
-        .del()
-        .then(function () {
-          return model;
-        });
-    })
-    .then(function (model) {
-      return model.destroy();
-    })
-    .then(function () {
+    //REMOVE USERS FROM A PROJECT   ///   if user is one of the users in if the project   ///   then execute the code below
+    projectController.put = function (req, res) {
+      // var userId = req.user.get('id');
       res.status(200).end();
-    });
-};
+    };
 
-module.exports = projectController;
+    /////////////////////////////////////////    DELETE    /////////////////////////////////////////
+    //DELETE A PROJECT
+    projectController.delete = function (req, res) {
+      models.Project
+        .query({
+          where: {
+            id: req.body.id
+          }
+        })
+        .fetch()
+        .then(function (model) {
+          return db('projects_users')
+            .where('project_id', '=', model.get('id'))
+            .del()
+            .then(function () {
+              return model;
+            });
+        })
+        .then(function (model) {
+          return model.destroy();
+        })
+        .then(function () {
+          res.status(200).end();
+        });
+    };
+
+    module.exports = projectController;
