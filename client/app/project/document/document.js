9315807e5b745c9cf6c711239eb4c0c3f6059815
thejsj <jorge.silva@thejsj.com>
2014-12-31 10:54:08 -0800

    Add document hashes to call up documents

--- client/app/project/document/document.js
+++ client/app/project/document/document.js
@@ -1,10 +1,11 @@
-/*global angular:true, bCrypt:true */
+/*global angular:true, Hashes:true, CodeMirror:true */
 /*jshint browser:true */
 'use strict';
 angular.module('code.document', ['ui.router'])
   .controller('documentController', function ($scope, $state, $stateParams) {
-    console.log('Document ID: ', $stateParams.projectName);
-    console.log('File Name: ', $stateParams.documentName);
+    $scope.projectName = $stateParams.projectName;
+    $scope.documentName = $stateParams.documentName;
+    // Setup Code Editor
     var cm = CodeMirror.fromTextArea(document.getElementById('pad'), {
       mode: 'javascript',
       value: 'alert(\'hello world\')',
@@ -12,19 +13,18 @@ angular.module('code.document', ['ui.router'])
       matchBrackets: true,
       theme: 'paraiso-dark'
     });
-
-    bCrypt.hashpw('hello', 'salt', function (hash) {
-      console.log('hash: ', hash);
-    });
-
-    var elem = document.getElementById('pad');
     var ws = new WebSocket('ws://' + window.location.hostname + ':8007'); // This should be dynamic
     var sjs = new window.sharejs.Connection(ws);
-    var collectionName = 'documents'; // project name? This should not be static
-    var doc = sjs.get(collectionName, $stateParams.projectName);
+    // Connect to document
+    /**
+     * We use a hash to call up the document
+     * We do this in order to keep the `document` collection flat
+     */
+    var str = 'p-' + $scope.projectName + '-d' + $scope.documentName;
+    var documentHash = new Hashes.SHA256().hex(str);
+    var doc = sjs.get('documents', documentHash);
     doc.subscribe();
     doc.whenReady(function () {
-      console.log(doc);
       if (!doc.type) {
         doc.create('text');
       }
