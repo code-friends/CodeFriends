f3a25b0540216615f1a15a35a941f6d8b1712b6c
Doug Phung <phung.doug@gmail.com>
2014-12-31 15:53:49 -0800

    adds the chat back

--- client/app/project/chat/chat.js
+++ client/app/project/chat/chat.js
@@ -2,8 +2,9 @@
 'use strict';
 
 angular.module('code.chat', ['ui.router'])
-  .controller('chatController', function ($scope, $state, ngSocket, $stateParams) {
+  .controller('chatController', function ($scope, $state, ngSocket, $stateParams, Auth) {
     var roomID = $stateParams.projectName;
+    var username = Auth.userName;
     var ws = ngSocket('ws://localhost:8001');
 
     $scope.roomID = roomID;
@@ -34,6 +35,16 @@ angular.module('code.chat', ['ui.router'])
         }
       }
     });
+    $scope.sendChat = function () {
+      var params = {
+        type: 'message',
+        username: username,
+        roomID: roomID,
+        message: $scope.chatMessage
+      };
+      ws.send(params);
+      $scope.chatMessage = '';
+    };
   })
   .directive('ngEnter', function () {
     return function (scope, element, attrs) {
