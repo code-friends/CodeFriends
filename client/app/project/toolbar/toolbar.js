867d12aa0d5a8a9dc09e349956d87e6c139a80b8
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-30 21:11:46 -0800

    Add addUser to toolbar, not yet working with server

--- client/app/project/toolbar/toolbar.js
+++ client/app/project/toolbar/toolbar.js
@@ -1,5 +1,15 @@
 /*global angular:true */
 angular.module('code.toolbar', [])
-  .controller('toolbarController', function ($scope, $state) {
+  .controller('toolbarController', function ($scope, $state, $stateParams, $http) {
     console.log('toolbar controller initialized!');
+    $scope.addUser = function () {
+      console.log('called addUser()');
+      return $http.put('/api/project/addUser/', { //correct path?
+          file_name: $scope.newUser,
+          project_name: $stateParams.projectName, // Where can we get this from?
+        })
+        .then(function () {
+          console.log('Added User');
+        });
+    };
   });
