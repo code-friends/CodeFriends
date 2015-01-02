2cb0ba30096eb9ffdd18951dcda8593865ccf397
Catherine Bui <contactcatherinebui@gmail.com>
2015-01-02 14:55:32 -0800

    Add editor theme functionality and toolbar to project view

--- client/app/project/toolbar/toolbar.js
+++ client/app/project/toolbar/toolbar.js
@@ -1,7 +1,46 @@
 /*global angular:true */
 
 angular.module('code.toolbar', [])
-  .controller('toolbarController', function ($scope, $state, $stateParams, $http) {
+  .controller('toolbarController', function ($scope, $state, $stateParams, $http, ToolbarDocument) {
+    $scope.themes = [
+      'Default',
+      'Ambiance',
+      'Base16 Dark',
+      'Base16 Light',
+      'Blackboard',
+      'Cobalt',
+      'Eclipse',
+      'Elegant',
+      'Lesser Dark',
+      'Midnight',
+      'Monokai',
+      'Neat',
+      'Neo',
+      'Night',
+      'Paraiso Dark',
+      'Paraiso Light',
+      'Ruby Blue',
+      'Solarized Dark',
+      'Solarized Light',
+      'Twilight',
+      'Vibrant Ink',
+      'Xq Dark',
+      'Xq Light',
+      '3024 Day',
+      '3024 Night'
+    ];
+
+    var formatThemeName = function (theme) {
+      var theme = theme.toLowerCase();
+      if (theme.split(' ')[0] === 'solarized') return theme;
+      return theme.replace(' ', '-');
+    };
+
+    $scope.changeEditorTheme = function (event) {
+      ToolbarDocument.changeTheme(formatThemeName(event.target.innerText));
+    };
+
+
     $scope.addUser = function () {
       return $http.put('api/project/addUser', {
           newUserName: $scope.newUser,
