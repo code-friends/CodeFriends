1cfe84664af886520441b6822413757aa6d3e8f9
Chase Ellsworth <chaseme3@gmail.com>
2015-01-05 21:00:12 -0800

    adds file upload from client to server to database 12

--- server/download/downloadController.js
+++ server/download/downloadController.js
@@ -11,10 +11,42 @@ var backend = require('../liveDbClient');
 var fileController = require('../file/fileController');
 
 var downloadController = {
+
 	downloadFile: function (req, res) {
-		console.log('req: ', req);
-		console.log('req.params: ', req.params);
+		var parsedUrl = req.params[0].split('/');
+		var projectId = parsedUrl[1];
+		var fileName = parsedUrl[3];
+		console.log('projectId: ', projectId, ' fileName: ', fileName);
+
+		var str = 'p-' + projectName + '-d' + documentName;
+		var documentHash = new Hashes.SHA256().hex(str);
+		// console.log('fileContent:', fileContent);
+		// console.log(documentHash);
+		backend.submit('documents', documentHash, {
+				create: {
+					type: 'text',
+					data: fileContent
+				}
+			},
+			function (err, version, transformedByOps, snapshot) {
+				if (err) {
+					console.log('ERROR: ', err);
+				}
+				console.log('version: ', version);
+				console.log('transformedByOps: ', transformedByOps);
+				console.log('snapshot: ', snapshot);
+				var fileInfo = {
+					projectName: projectName,
+					fileName: documentName,
+					type: 'file', ///need to make flexible to take folders too
+					path: '',
+					userId: userId
+				};
+			});
+
+
+
 	}
-};
+}
 
 module.exports = downloadController;
