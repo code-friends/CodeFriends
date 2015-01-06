1beea4a20ebcecf9691fe0486b89d8f48eac39ca
Chase Ellsworth <chaseme3@gmail.com>
2015-01-05 16:54:25 -0800

    adds file upload from client to server to database 9

---
+++ server/download/downloadRouter.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var downloadController = require('./downloadController.js');
+
+var downloadRouter = express.Router();
+
+downloadRouter.get('/', downloadController.downloadFile);
+
+module.exports = downloadRouter;
