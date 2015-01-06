51d664c5f7f76c706c0bb816a429d3d091ffa476
Chase Ellsworth <chaseme3@gmail.com>
2015-01-05 17:33:02 -0800

    adds file upload from client to server to database 10

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ gulp.task('js', function () {
       './client/app/project/toolbar/toolbar.js',
       './client/app/templates/mainHeader.js',
       './client/app/project/uploads/uploadsCtrl.js',
+      './client/app/project/downloads/downloadsCtrl.js',
       './client/app/app.js',
       './client/lib/ngSocket/dist/ngSocket.js',
       './client/lib/moment/moment.js',
