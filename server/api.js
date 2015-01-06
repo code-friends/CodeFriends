78b7c9769dd920c296008d16aac9641666bfedc3
Chase Ellsworth <chaseme3@gmail.com>
2015-01-05 16:54:25 -0800

    adds file upload from client to server to database 9

--- server/api.js
+++ server/api.js
@@ -3,6 +3,7 @@ var projectRouter = require('./project/projectRoutes');
 var userRouter = require('./user/userRoutes');
 var fileRouter = require('./file/fileRouter');
 var uploadRouter = require('./upload/uploadRouter');
+var downloadRouter = require('./download/downloadRouter');
 var express = require('express');
 
 var apiRouter = express.Router();
@@ -11,5 +12,6 @@ apiRouter.use('/file', fileRouter);
 apiRouter.use('/project', projectRouter);
 apiRouter.use('/user', userRouter);
 apiRouter.use('/upload', uploadRouter);
+apiRouter.use('/download', downloadRouter);
 
 module.exports = apiRouter;
---
+++ server/download/downloadController.js
@@ -0,0 +1,19 @@
+'use strict';
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
+var mongoClient = Promise.promisifyAll(require('mongodb').MongoClient);
+var Q = require('q');
+var moment = require('moment');
+var Hashes = require('jshashes');
+var multiparty = require('multiparty');
+var backend = require('../liveDbClient');
+var fileController = require('../file/fileController');
+
+var downloadController = {
+	downloadFile: function (req, res) {
+
+	}
+};
+
+module.exports = downloadController;
