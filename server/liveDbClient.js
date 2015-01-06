c40c484ca7f6184000319ca7b6702f5c6b1a5e7a
Chase Ellsworth <chaseme3@gmail.com>
2015-01-06 11:38:43 -0800

    adds file upload from client to server to database 15

--- server/liveDbClient.js
+++ server/liveDbClient.js
@@ -1,8 +1,9 @@
+var Promise = require('bluebird');
 var liveDBMongoClient = require('livedb-mongo');
 var livedb = require('livedb');
 var db = liveDBMongoClient('mongodb://localhost:27017/codeFriends?auto_reconnect', {
 	safe: true
 });
-var backend = livedb.client(db);
+var backend = Promise.promisifyAll(livedb.client(db));
 
 module.exports = backend;
