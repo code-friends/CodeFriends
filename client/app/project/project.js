d2874c6dde90edf33aa0ab70f64e3ebb48e77ddf
Catherine Bui <contactcatherinebui@gmail.com>
2015-01-07 19:46:05 -0800

    Add file and folder to specific path, edit file structure html and css, add styling

--- client/app/project/project.js
+++ client/app/project/project.js
@@ -2,11 +2,9 @@
 /*jshint browser:true */
 'use strict';
 angular.module('code.project', ['ui.router'])
-  .controller('projectController', function ($scope, ngSocket, $state, $stateParams, $http, Auth, Files, Projects, documentFactory) {
+  .controller('projectController', function ($scope, $state, $stateParams, $http, Auth, Files, ProjectFactory, documentFactory) {
     Auth.isLoggedIn();
     $scope.username = null;
-    $scope.files = [];
-    $scope.currentProjectId = null;
 
     var ws = ngSocket('ws://' + window.location.hostname + ':' + window.config.ports.chat);
 
@@ -22,61 +20,23 @@ angular.module('code.project', ['ui.router'])
         $scope.username = userName;
       });
 
-    $scope.getAllFiles = function () {
-      return Projects.getProject($stateParams.projectName)
+    // saves current project id, current project name and files to $scope
+    $scope.getProject = function () {
+      return ProjectFactory.getProject($stateParams.projectName)
         .then(function (project) {
           $scope.currentProjectId = project.id;
           $scope.currentProjectName = project.project_name; //change eventually to project id
           $scope.files = project.files;
-          console.log($scope.files);
-          return $scope.files;
+          console.log('files in project on projectController loading', $scope.files);
         })
         .catch(function (err) {
           console.log('Could Not Get Project', err);
         });
     };
 
-    $scope.Projects = Projects;
-    $scope.updateName = function (name) {
-      $scope.Projects.updateName(name, function () {
-        console.log('Projects.filename', Projects.filename);
-      });
-    };
-
     $scope.goToHome = function () {
       $state.go('home');
     };
 
-    $scope.addNewFile = function () {
-      return Files.addNewFile($scope.newFileName, $stateParams.projectName)
-        .then(function (files) {
-          $scope.files = files;
-          var cm = CodeMirror.fromTextArea(document.getElementById('pad'), {
-            mode: 'javascript',
-            value: 'function(){}',
-            lineNumbers: true,
-            matchBrackets: true,
-            theme: 'solarized dark'
-          });
-          console.log('doug codemirror', cm);
-          console.log($scope.newFileName, $stateParams.projectName, cm);
-          documentFactory.goToDocument($scope.newFileName, $stateParams.projectName, cm);
-        });
-
-      Files.getAllFiles($stateParams.projectName)
-        .then(function (files) {
-          $scope.files = files;
-        });
-
-      return $http.post('/api/file', {
-          file_name: $scope.newFileName,
-          project_name: $stateParams.projectName,
-          type: 'file',
-          parent_file: null
-        })
-        .then(function () {
-          return $scope.getAllFiles();
-        });
-    };
-    $scope.getAllFiles();
+    $scope.getProject();
   });
