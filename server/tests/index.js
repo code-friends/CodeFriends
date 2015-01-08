a2e141c20d2a70cfd5dec13f4b63708cdbbfce9a
thejsj <jorge.silva@thejsj.com>
2015-01-07 16:41:23 -0800

    Fix projectDownload

--- server/tests/index.js
+++ server/tests/index.js
@@ -1,11 +1,8 @@
 /*global describe:true, before: true, after: true */
 'use strict';
 
-var config = require('config');
-var Promise = require('bluebird');
-var should = require('should');
 var db = require('../db');
-var mongoClient =  Promise.promisifyAll(require('mongodb').MongoClient);
+var deleteAllDatabases = require('../deleteAllDatabases');
 
 describe('Code Friends', function () {
 
@@ -21,33 +18,8 @@ describe('Code Friends', function () {
 
     // Delete All Test Tables
   after(function (done) {
-    db.schema.dropTable('projects_users')
+    deleteAllDatabases()
       .then(function () {
-        return Promise.all([
-          db.schema.dropTable('users'),
-          db.schema.dropTable('projects'),
-        ]);
-      })
-      .then(function () {
-        console.log('Deleting All MySQL Tables');
-        return true;
-      })
-      .then(function () {
-        return mongoClient.connectAsync(config.get('mongo'))
-          .then(function (db) {
-              var projectCollection = Promise.promisifyAll(db.collection('project_file_structre'));
-              return projectCollection.removeAsync();
-          })
-          .then(function () {
-            console.log('Deleting All Mongo Collections');
-            return true;
-          })
-          .catch(function (err) {
-            console.log('Error Deleting Mongo Collection', err);
-          });
-      })
-      .then(function () {
-        console.log('Deleting Database Done');
         done();
       })
       .catch(function (err) {
