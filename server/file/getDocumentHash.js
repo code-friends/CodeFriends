309a40deb705389d56cfb9a1e203db814d90ba17
Chase Ellsworth <chaseme3@gmail.com>
2015-01-09 17:19:18 -0800

    rewrites server tests 4

--- server/file/getDocumentHash.js
+++ server/file/getDocumentHash.js
@@ -14,6 +14,7 @@ var Hashes = require('jshashes');
  * @return <Promise> -> <String> (documentHash)
  */
 var getDocumentHash = function (projectNameOrId, documentPath) {
+
   if (typeof documentPath !== 'string') throw new Error('Document Path is Not A String');
   return new Q()
     .then(function () {
@@ -48,9 +49,21 @@ var getDocumentHash = function (projectNameOrId, documentPath) {
         documentPath = '/' + documentPath;
       }
       var str = 'p-' + projectId + '-d' + documentPath;
+      console.log('GET HASH projectNameOrId: ', projectId);
+      console.log('GET HASH documentPath: ', documentPath);
+      console.log('GET HASH str: ', str);
       var documentHash = new Hashes.SHA256().hex(str);
+      console.log('GET HASH documentHash: ', documentHash);
       return documentHash;
     });
 };
 
 module.exports = getDocumentHash;
+
+// 40d5ea3b220fa8a50fee32c32c2579a08f9f26df6923a614c493f4bbc5ac627f
+// 40d5ea3b220fa8a50fee32c32c2579a08f9f26df6923a614c493f4bbc5ac627f
+// 40d5ea3b220fa8a50fee32c32c2579a08f9f26df6923a614c493f4bbc5ac627f
+// 40d5ea3b220fa8a50fee32c32c2579a08f9f26df6923a614c493f4bbc5ac627f
+// 40d5ea3b220fa8a50fee32c32c2579a08f9f26df6923a614c493f4bbc5ac627f
+// 40d5ea3b220fa8a50fee32c32c2579a08f9f26df6923a614c493f4bbc5ac627f
+// 40d5ea3b220fa8a50fee32c32c2579a08f9f26df6923a614c493f4bbc5ac627f
