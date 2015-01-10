d6cd627e9324bdea971c72d833473be7fd270d8d
Doug Phung <phung.doug@gmail.com>
2015-01-09 17:13:32 -0800

    defaults to first file in project

--- client/app/project/project.js
+++ client/app/project/project.js
@@ -2,7 +2,7 @@
 /*jshint browser:true */
 'use strict';
 angular.module('code.project', ['ui.router'])
-  .controller('projectController', function ($scope, $state, $stateParams, $http, Auth, Files, ProjectFactory, documentFactory, SocketFactory) {
+  .controller('projectController', function ($rootScope, $scope, $state, $stateParams, $http, Auth, Files, ProjectFactory, documentFactory, SocketFactory) {
     // Auth.username should now be here, since we're making the http request
     // before getting here
     $scope.username = Auth.userName;
@@ -20,6 +20,20 @@ angular.module('code.project', ['ui.router'])
           $scope.currentProjectId = project.id;
           $scope.currentProjectName = project.project_name; //change eventually to project id
           $scope.files = project.files;
+          if (typeof $state.params.documentName === 'undefined') {
+            var firstFile;
+            angular.forEach(project.files, function (file) {
+              if (file.type === 'file') {
+                firstFile = file;
+                return;
+              }
+            });
+            $state.go('document', {
+              'projectName': $state.params.projectName,
+              'projectId': $state.params.projectId,
+              'documentPath': firstFile.path
+            });
+          }
           return $scope.files;
         })
         .catch(function (err) {
