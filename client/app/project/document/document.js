9bf954e0a93c8dd6099f0eaab7af04c63c32e073
Doug Phung <phung.doug@gmail.com>
2015-01-12 20:03:57 -0800

    adds a run button that evals code

--- client/app/project/document/document.js
+++ client/app/project/document/document.js
@@ -2,50 +2,65 @@
 /*jshint browser:true */
 'use strict';
 angular.module('code.document', ['ui.router'])
-  .controller('documentController', function ($scope, $stateParams, ToolbarDocument, documentFactory) {
+  .controller('documentController', function ($rootScope, $scope, $stateParams, ToolbarDocument, documentFactory) {
+    $rootScope.$on('compile code', function () {
+      console.log($scope.cm.getValue());
+      var fn = window[$scope.cm.getValue()];
+      eval($scope.cm.getValue());
+    });
     $scope.projectName = $stateParams.projectName;
     $scope.documentPath = $stateParams.documentPath;
     $scope.theme = ToolbarDocument.theme;
+
     // Setup Code Editor
-    var cm = CodeMirror.fromTextArea(document.getElementById('pad'), {
+    $scope.cm = CodeMirror.fromTextArea(document.getElementById('pad'), {
       mode: 'javascript',
-      value: 'alert(\'hello world\')',
       lineNumbers: true,
       matchBrackets: true,
       theme: 'solarized dark'
     });
 
-    documentFactory.goToDocument($scope.projectName, $scope.documentPath, cm);
+    documentFactory.goToDocument($scope.projectName, $scope.documentPath, $scope.cm);
     // listens for theme variable changed in ToolbarDocument factory broadcasted by $rootScope
     $scope.$on('theme:changed', function (event, theme) {
-      cm.setOption('theme', theme);
+      $scope.cm.setOption('theme', theme);
     });
 
   })
   .factory('documentFactory', function (Projects, $state, $stateParams) {
     return {
       goToDocument: function (projectName, filePath, codeMirror) {
-        var ws = new WebSocket('ws://' + window.location.hostname + ':' + window.config.ports.editor);
-        var sjs = new window.sharejs.Connection(ws);
-        /**
-         * Look in getDocumentHash before changing this
-         */
-        if (filePath[0] !== '/') {
-          filePath = '/' + filePath;
+          var ws = new WebSocket('ws://' + window.location.hostname + ':' + window.config.ports.editor);
+          var sjs = new window.sharejs.Connection(ws);
+          /**
+           * Look in getDocumentHash before changing this
+           */
+          if (filePath[0] !== '/') {
+            filePath = '/' + filePath;
+          }
+          var str = 'p-' + $stateParams.projectId + '-d' + filePath;
+          var filePathHash = new Hashes.SHA256().hex(str);
+          var doc = sjs.get('documents', filePathHash);
+          doc.subscribe();
+          doc.whenReady(function () {
+            if (!doc.type) {
+              doc.create('text');
+            }
+            if (doc.type && doc.type.name === 'text') {
+              doc.attachCodeMirror(codeMirror);
+            }
+          });
         }
-        var str = 'p-' + $stateParams.projectId + '-d' + filePath;
-        var filePathHash = new Hashes.SHA256().hex(str);
-        var doc = sjs.get('documents', filePathHash);
-        doc.subscribe();
-        doc.whenReady(function () {
-          if (!doc.type) {
-            doc.create('text');
-          }
-          if (doc.type && doc.type.name === 'text') {
-            doc.attachCodeMirror(codeMirror);
-          }
-        });
-
-      }
+        // getDocumentText = function (projectName, filePath, codeMirror) {
+        //   var ws = new WebSocket('ws://' + window.location.hostname + ':' + window.config.ports.editor);
+        //   var sjs = new window.sharejs.Connection(ws);
+        //   if (filePath[0] !== '/') {
+        //     filePath = '/' + filePath;
+        //   }
+        //   var str = 'p-' + $stateParams.projectId + '-d' + filePath;
+        //   var filePathHash = new Hashes.SHA256().hex(str);
+        //   var doc = sjs.get('documents', filePathHash);
+        //   return doc.getText();
+        // }
     };
   });
