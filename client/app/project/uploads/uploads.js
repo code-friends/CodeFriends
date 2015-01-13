88389433fd89a6d8a5530825aa31b7da56d48933
Catherine Bui <contactcatherinebui@gmail.com>
2015-01-13 09:54:56 -0800

    Refactor uploads.js

---
+++ client/app/project/uploads/uploads.js
@@ -0,0 +1,38 @@
+/*global angular:true */
+(function () {
+  'use strict';
+
+  angular.module('codeFriends.uploads', [])
+    .controller('uploadsController', ['$scope', '$state', '$stateParams', '$http', '$upload', function ($scope, $state, $stateParams, $http, $upload) {
+
+      $scope.onFileSelect = function (files) {
+        var uploadFile = function (fileIndex) {
+          return $upload.upload({
+              method: 'POST',
+              url: '/api/file/upload',
+              data: {
+                filePath: files[fileIndex].name,
+                projectName: $stateParams.projectName,
+              },
+              file: files[fileIndex]
+            })
+            .then(function (newFileStructure) {
+              if (files.length > fileIndex + 1) {
+                return uploadFile(fileIndex + 1);
+              } else {
+                return true;
+              }
+            })
+            .catch(function (error) {
+              console.log('Error Uploading File: ', error);
+            });
+        };
+        uploadFile(0)
+          .then(function () {
+            console.log('All Files Uploaded');
+            // TODO: Add Socket .send
+          });
+      };
+      //get request to reload with updated files
+    }]);
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('js', function () {
       './client/app/templates/mainHeader.js',
       './client/app/templates/modalCreateProject.js',
       './client/app/templates/modalAddToProject.js',
-      './client/app/project/uploads/uploadsCtrl.js',
+      './client/app/project/uploads/uploads.js',
       './client/app/project/downloads/downloadsCtrl.js',
       './client/app/app.js',
       './client/lib/ngSocket/dist/ngSocket.js',
