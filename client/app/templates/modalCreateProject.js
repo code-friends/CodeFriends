b4dfc52c627f07f41ca82fbddf5d28caae482034
Catherine Bui <contactcatherinebui@gmail.com>
2015-01-12 18:54:12 -0800

    THIS ONE IS THE ONE THAT ACTUALLY WORKS

--- client/app/templates/modalCreateProject.js
+++ client/app/templates/modalCreateProject.js
@@ -50,21 +50,3 @@
   //   console.log('End createProjectModalController');
   // }
 })();
-
-// .controller('createProjectModalController', function ($scope, $modalInstance, $upload, ProjectsFactory) {
-//   $scope.files = null;
-//   $scope.onFileSelect = function (files) {
-//     $scope.files = files;
-//   };
-
-//   $scope.closeModal = function () {
-//     console.log('closeModal');
-//     console.log($scope.files);
-//     if ($scope.newProjectName !== undefined) {
-//       ProjectsFactory.createProject($scope.newProjectName, $scope.files)
-//         .then(function () {
-//           $modalInstance.close();
-//         });
-//     }
-//   };
-// });
