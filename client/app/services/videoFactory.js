665904ddfdb8890a9a853f44beb77ff543a452a8
thejsj <jorge.silva@thejsj.com>
2015-01-16 14:23:38 -0800

    Make webrtc a global variable

---
+++ client/app/services/videoFactory.js
@@ -0,0 +1,32 @@
+/*global angular:true*/
+(function () {
+  'use strict';
+
+  angular.module('codeFriends.services')
+    .factory('VideoFactory', VideoFactory);
+
+  function VideoFactory() {
+
+    var webrtc = new SimpleWebRTC({
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos
+      remoteVideosEl: '',
+      // immediately ask for camera access
+      autoRequestMedia: false,
+      debug: false,
+      detectSpeakingEvents: true,
+      adjustPeerVolume: true,
+      autoAdjustMic: true
+    });
+    window.webrtc = webrtc;
+
+    webrtc.startVideo = function () {
+      webrtc.startLocalVideo();
+    };
+
+    return webrtc;
+
+  }
+
+})();
