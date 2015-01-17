b3c8515dfa9a2ea8779b5c5e3e413e4984eff134
Doug Phung <phung.doug@gmail.com>
2015-01-17 10:20:46 -0800

    rebased

--- client/app/services/videoFactory.js
+++ client/app/services/videoFactory.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-/*global angular:true*/
-(function () {
-  'use strict';
-
-  angular.module('codeFriends.services')
-    .factory('VideoFactory', VideoFactory);
-
-  function VideoFactory() {
-
-    console.log('Start SimpleWebRTC');
-    var webrtc = new SimpleWebRTC({
-      // the id/element dom element that will hold "our" video
-      localVideoEl: 'localVideo',
-      // the id/element dom element that will hold remote videos
-      remoteVideosEl: 'remotes',
-      // immediately ask for camera access
-      url: 'http://docker.dev:9003',
-      autoRequestMedia: true,
-      detectSpeakingEvents: false,
-      debug: true,
-    });
-    window.webrtc = webrtc;
-
-    webrtc.startVideo = function () {
-      webrtc.startLocalVideo();
-    };
-
-    return webrtc;
-
-  }
-
-})();
-=======
   /*global angular:true, moment:true, _:true */
 
   /*global angular:true*/
@@ -81,4 +47,3 @@
       return iceComm;
     }
   })();
->>>>>>> file structure refreshes and video stream stopped going to projects
