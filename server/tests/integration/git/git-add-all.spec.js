8d726eb73f53309dc77aebff395606ac90b0c139
Chase Ellsworth <chaseme3@gmail.com>
2015-05-20 22:22:31 -0700

    feat(server): skeleton test files for git funcs
    skeleton test files and tests for gitAddAll(),
    gitCommit(), and gitPush().

---
+++ server/tests/integration/git/git-add-all.spec.js
@@ -0,0 +1,32 @@
+/*global describe:true, it:true, before: true */
+'use strict';
+
+var Promise = require('bluebird');
+var request = require('supertest-as-promised');
+var expect = require('chai').expect;
+var app = require('../../index');
+var agent = request.agent(app);
+var login = require('./login')(agent);
+var fs = Promise.promisifyAll(require('fs'));
+
+describe('gitAddAll', function () {
+
+// agent persists cookies and sessions
+
+  var projectName = 'basketball';
+  before(function (done) {
+    login()
+      .then(function () {
+        done();
+      });
+  });
+
+  it('should ', function (done) {
+    
+  });
+
+  it('should ', function (done) {
+    
+  });
+
+});
